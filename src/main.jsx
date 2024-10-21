f6f01dbac7f2a63a5c488993a9ab4632d6f66210
Anstro <anstrosolano@gmail.com>
2024-10-20 21:45:23 -0500

    Eliminando componentes de Registro

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './layouts/Layout'
 import Login from './views/Login'
 import Home from './views/Home'
-import Register from './views/Register'
 import './index.css'
 
 const router = createBrowserRouter([
@@ -19,15 +18,10 @@ const router = createBrowserRouter([
       {
         path: '',
         element: <Home />
-      }, 
-      {
-        path: '/register',
-        element: <Register/>
-      }
+      },
     ]
   }
 ])
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
